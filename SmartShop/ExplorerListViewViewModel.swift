099158ad858201a944ed2b54a2c15ebf18fd26d2
Anton Stafeev <stafeevt@mail.ru>
2025-02-11 21:37:31 +0500

    Add ExplorerListViewViewModel

--- SmartShop/ExplorerListViewViewModel.swift
+++ SmartShop/ExplorerListViewViewModel.swift
@@ -5,4 +5,31 @@
 //  Created by Антон Стафеев on 11.02.2025.
 //
 
-import Foundation
+import UIKit
+
+final class ExplorerListViewViewModel: NSObject {
+    private var products: [Product] = []
+}
+
+
+// MARK: - UICollectionViewDataSource
+extension ExplorerListViewViewModel: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ExplorerListCollectionViewCell.identifier, for: indexPath) as? ExplorerListCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension ExplorerListViewViewModel: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.bounds.width - 30) / 2
+        return CGSize(width: width, height: width * 1.2)
+    }
+}
