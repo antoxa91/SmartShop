4e75a28a9832ae254d169a7ade6a202657529b7d
Anton Stafeev <stafeevt@mail.ru>
2025-02-12 00:14:45 +0500

    Add searchTextField, basketView

--- SmartShop/ExplorerListView.swift
+++ SmartShop/ExplorerListView.swift
@@ -8,15 +8,19 @@
 import UIKit
 
 final class ExplorerListView: UIView {
+    private enum Constants {
+        static let sectionInset: CGFloat = 10
+        static let elementInset: CGFloat = 8
+        static let elementHeight: CGFloat = 40
+    }
     
     private let viewModel = ExplorerListViewViewModel()
-    private let sectionInset: CGFloat = 10
     
     // MARK: Private UI Properties
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
-        layout.sectionInset = UIEdgeInsets(top: 0, left: sectionInset, bottom: sectionInset, right: sectionInset)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: Constants.sectionInset, bottom: Constants.sectionInset, right: Constants.sectionInset)
         
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -24,6 +28,9 @@ final class ExplorerListView: UIView {
                                 forCellWithReuseIdentifier: ExplorerListCollectionViewCell.identifier)
         return collectionView
     }()
+    
+    private lazy var searchTextField = SearchTextField()
+    private lazy var basketView = BasketView()
     
     // MARK: Init
     override init(frame: CGRect) {
@@ -45,13 +52,23 @@ final class ExplorerListView: UIView {
     }
     
     private func setupSubviews() {
-        addSubview(collectionView)
+        addSubviews(collectionView, searchTextField, basketView)
         translatesAutoresizingMaskIntoConstraints = false
     }
     
     private func setConstraints() {
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            searchTextField.topAnchor.constraint(equalTo: topAnchor),
+            searchTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.elementInset),
+            searchTextField.trailingAnchor.constraint(equalTo: basketView.leadingAnchor, constant: -Constants.elementInset),
+            searchTextField.heightAnchor.constraint(equalToConstant: Constants.elementHeight),
+            
+            basketView.topAnchor.constraint(equalTo: topAnchor),
+            basketView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Constants.elementInset),
+            basketView.widthAnchor.constraint(equalToConstant: Constants.elementHeight),
+            basketView.heightAnchor.constraint(equalToConstant: Constants.elementHeight),
+            
+            collectionView.topAnchor.constraint(equalTo: searchTextField.bottomAnchor, constant: Constants.elementInset),
             collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
             collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
