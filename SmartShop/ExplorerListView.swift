23926f078960fa4a96135ded8cefbd41288edf05
Anton Stafeev <stafeevt@mail.ru>
2025-02-11 21:33:44 +0500

    Add ExplorerListView

--- SmartShop/ExplorerListView.swift
+++ SmartShop/ExplorerListView.swift
@@ -5,4 +5,56 @@
 //  Created by Антон Стафеев on 11.02.2025.
 //
 
-import Foundation
+import UIKit
+
+final class ExplorerListView: UIView {
+    
+    private let viewModel = ExplorerListViewViewModel()
+    private let sectionInset: CGFloat = 10
+    
+    // MARK: Private UI Properties
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: sectionInset, bottom: sectionInset, right: sectionInset)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(ExplorerListCollectionViewCell.self,
+                                forCellWithReuseIdentifier: ExplorerListCollectionViewCell.identifier)
+        return collectionView
+    }()
+    
+    // MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubviews()
+        setupCollectionView()
+        setConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Setup
+    private func setupCollectionView() {
+        collectionView.dataSource = viewModel
+        collectionView.delegate = viewModel
+    }
+    
+    private func setupSubviews() {
+        addSubview(collectionView)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
