773bd1037dd4d8d4009b0507aca2b685b5f35cc3
Anton Stafeev <stafeevt@mail.ru>
2025-02-12 11:29:01 +0500

    add configureNavigationBarAppearance()

--- SmartShop/SupportingFiles/AppDelegate.swift
+++ SmartShop/SupportingFiles/AppDelegate.swift
@@ -9,17 +9,29 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     var navigationController: UINavigationController?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         navigationController = UINavigationController(rootViewController: ExplorerViewController())
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         
+        configureNavigationBarAppearance()
+        
         return true
+    }
+    
+    private func configureNavigationBarAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = AppColorEnum.appBackground.color
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
     }
 }
 
