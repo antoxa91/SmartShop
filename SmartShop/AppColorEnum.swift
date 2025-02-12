41234f6b41058ca0037c9cd99405107cefdee99f
Anton Stafeev <stafeevt@mail.ru>
2025-02-12 11:16:53 +0500

    Update colors

--- SmartShop/AppColorEnum.swift
+++ SmartShop/AppColorEnum.swift
@@ -10,16 +10,25 @@ import UIKit
 enum AppColorEnum {
     case appBackground
     case cellBackground
-    case label
+    case tint
+    case top
+    case collectionView
+    case tfBg
     
     var color: UIColor {
         switch self {
         case .appBackground:
-            return .systemBackground
+            return UIColor(hex: "#025F46")
         case .cellBackground:
-            return .secondarySystemBackground
-        case .label:
-            return .label
+            return UIColor(hex: "#BBE199")
+        case .tint:
+            return UIColor(hex: "#D4F3EA")
+        case .top:
+            return UIColor(hex: "#015F46")
+        case .collectionView:
+            return UIColor(hex: "#E3F8CC")
+        case .tfBg:
+            return UIColor(hex: "#024F43")
         }
     }
 }
---
+++ SmartShop/UIColor + Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor + Extension.swift
+//  SmartShop
+//
+//  Created by Антон Стафеев on 12.02.2025.
+//
+
+import Foundation
