27ee6400dfe9b08de5b1d6a9c0c442bdf68cacfe
Anton Stafeev <stafeevt@mail.ru>
2025-02-13 18:43:38 +0500

    Update project files

--- FilterViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  FilterViewController.swift
-//  SmartShop
-//
-//  Created by Антон Стафеев on 12.02.2025.
-//
-
-import Foundation
--- SmartShop/Network/FilterParameters.swift
+++
@@ -1,8 +0,0 @@
-//
-//  FilterParameters.swift
-//  SmartShop
-//
-//  Created by Антон Стафеев on 13.02.2025.
-//
-
-import Foundation
