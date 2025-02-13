71128859b90ededc57e75f866c76f7566de5b38f
Anton Stafeev <stafeevt@mail.ru>
2025-02-13 14:48:47 +0500

    Add FilterTF.swift

--- SmartShop/Filter/FilterTF.swift
+++ SmartShop/Filter/FilterTF.swift
@@ -5,4 +5,27 @@
 //  Created by Антон Стафеев on 13.02.2025.
 //
 
-import Foundation
+import UIKit
+
+final class FilterTF: UITextField {
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        self.placeholder = placeholder
+        setup()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        self.borderStyle = .roundedRect
+        self.backgroundColor = AppColorEnum.tint.color
+        self.textColor = AppColorEnum.top.color
+        self.keyboardType = .numberPad
+        self.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        self.setContentCompressionResistancePriority(.required, for: .horizontal)
+        self.textAlignment = .justified
+    }
+}
