15c7d1a0d7a75a09e8d5306ee27b49ce171e8087
Anton Stafeev <stafeevt@mail.ru>
2025-02-13 16:43:28 +0500

    Add FilterViewController.swift

--- SmartShop/Filter/FilterViewController.swift
+++ SmartShop/Filter/FilterViewController.swift
@@ -1,3 +1,10 @@
+//
+//  FilterViewController.swift
+//  SmartShop
+//
+//  Created by Антон Стафеев on 13.02.2025.
+//
+
 import UIKit
 
 protocol FilterDelegate: AnyObject {
@@ -5,145 +12,60 @@ protocol FilterDelegate: AnyObject {
 }
 
 final class FilterViewController: UIViewController {
+    // MARK: Properties
+    private lazy var filterView = FilterView()
+    private lazy var categoryManager = CategoryManager(containerView: filterView.categoryButtons)
+    private let networkService: ProductsLoader
     
-    // MARK: - UI Properties
-    private lazy var priceTextField = FilterTF(placeholder: "Enter price")
-    private lazy var minPriceTextField = FilterTF(placeholder: "Min price")
-    private lazy var maxPriceTextField = FilterTF(placeholder: "Max price")
-    
-    private lazy var priceLabel = FilterLabel(text: "Price")
-    private lazy var priceFromLabel = FilterLabel(text: "Price from")
-    private lazy var priceToLabel = FilterLabel(text: "to")
-    private lazy var categoryLabel = FilterLabel(text: "Categories")
-
-    private lazy var applyButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Apply Filters", for: .normal)
-        button.titleLabel?.font = .boldSystemFont(ofSize: 18)
-        button.setTitleColor(AppColorEnum.tint.color, for: .normal)
-        button.backgroundColor = AppColorEnum.top.color
-        button.layer.borderColor = AppColorEnum.tfBg.color.cgColor
-        button.layer.borderWidth = 2
-        button.layer.cornerRadius = 12
-        button.addTarget(self, action: #selector(applyFilters), for: .touchUpInside)
-        return button
-    }()
-
     weak var delegate: FilterDelegate?
     
-    // MARK: - Lifecycle
+    // MARK: Init
+    init(networkService: ProductsLoader) {
+        self.networkService = networkService
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        view = filterView
+    }
+    
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
+        setupActions()
+        loadCategories()
     }
     
-    // MARK: - Setup UI
-    private func setupUI() {
-        view.backgroundColor = AppColorEnum.cellBackground.color
-        
-        let priceStackView = createHorizontalStackView(arrangedSubviews: [priceLabel, priceTextField])
-        
-        let minMaxPriceStackView = createHorizontalStackView(arrangedSubviews: [priceFromLabel, minPriceTextField, priceToLabel, maxPriceTextField])
-        
-        // Center categoryLabel
-        let categoryContainerView = UIView()
-        categoryContainerView.addSubview(categoryLabel)
-        categoryLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            categoryLabel.centerXAnchor.constraint(equalTo: categoryContainerView.centerXAnchor),
-            categoryLabel.topAnchor.constraint(equalTo: categoryContainerView.topAnchor),
-            categoryLabel.bottomAnchor.constraint(equalTo: categoryContainerView.bottomAnchor)
-        ])
-        
-        let mainStackView = UIStackView(arrangedSubviews: [priceStackView, minMaxPriceStackView, categoryContainerView])
-        mainStackView.axis = .vertical
-        mainStackView.spacing = 20
-        mainStackView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(mainStackView)
-        
-        // Configure applyButton
-        applyButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(applyButton)
-        
-        NSLayoutConstraint.activate([
-            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            
-            applyButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            applyButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            applyButton.heightAnchor.constraint(equalToConstant: 50),
-            applyButton.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor, constant: -20)
-        ])
+    // MARK: Setup Actions
+    private func setupActions() {
+        filterView.setApplyButtonAction(target: self, action: #selector(applyFilters))
     }
     
-    private func createHorizontalStackView(arrangedSubviews: [UIView]) -> UIStackView {
-        let stackView = UIStackView(arrangedSubviews: arrangedSubviews)
-        stackView.axis = .horizontal
-        stackView.spacing = 10
-        stackView.distribution = .fill
-        stackView.alignment = .center
-        return stackView
-    }
-    
-    // MARK: - Actions
+    // MARK: Actions
     @objc private func applyFilters() {
-        var parameters = FilterParameters()
-        
-        if let priceText = priceTextField.text {
-            parameters.price = priceText
-        }
-        
-        if let minPriceText = minPriceTextField.text {
-            parameters.priceMin = minPriceText
-        }
-        
-        if let maxPriceText = maxPriceTextField.text {
-            parameters.priceMax = maxPriceText
-        }
-        
+        let parameters = FilterParameters(
+            price: filterView.priceText,
+            priceMin: filterView.minPriceText,
+            priceMax: filterView.maxPriceText,
+            categoryId: categoryManager.categoryId
+        )
         delegate?.applyFilters(parameters: parameters)
-        dismiss(animated: true, completion: nil)
-    }
-}
-
-final class FilterLabel: UILabel {
-    init(text: String) {
-        super.init(frame: .zero)
-        self.text = text
-        setup()
+        dismiss(animated: true)
     }
     
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setup() {
-        self.font = .boldSystemFont(ofSize: 17)
-        self.textColor = AppColorEnum.top.color
-        self.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-    }
-}
-
-final class FilterTF: UITextField {
-    init(placeholder: String) {
-        super.init(frame: .zero)
-        self.placeholder = placeholder
-        setup()
-    }
-    
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setup() {
-        self.borderStyle = .roundedRect
-        self.backgroundColor = AppColorEnum.tint.color
-        self.textColor = AppColorEnum.top.color
-        self.keyboardType = .numberPad
-        self.setContentHuggingPriority(.defaultLow, for: .horizontal)
-        self.setContentCompressionResistancePriority(.required, for: .horizontal)
+    // MARK: Load Categories
+    private func loadCategories() {
+        networkService.fetchInitialProducts { [weak self] in
+            DispatchQueue.main.async {
+                let categories = self?.networkService.products.map { $0.category } ?? []
+                self?.categoryManager.updateCategories(categories)
+            }
+        }
     }
 }
