8495f3e36cec5bc0138c5b38455fd77cdcffbaa6
Anton Stafeev <stafeevt@mail.ru>
2025-02-15 04:14:28 +0500

    Add ProductLabel

--- SmartShop/ProductLabel.swift
+++ SmartShop/ProductLabel.swift
@@ -5,4 +5,20 @@
 //  Created by Антон Стафеев on 15.02.2025.
 //
 
-import Foundation
+import UIKit
+
+final class ProductLabel: UILabel {
+    init(text: String = "", textColor: UIColor = .black, font: UIFont, numberOfLines: Int = 1) {
+        super.init(frame: .zero)
+        self.text = text
+        self.textColor = textColor
+        self.font = font
+        self.numberOfLines = numberOfLines
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
