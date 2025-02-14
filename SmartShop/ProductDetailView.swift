fbf0a823ae202e8092cf6067c81f34b5e00ca9c7
Anton Stafeev <stafeevt@mail.ru>
2025-02-15 03:53:32 +0500

    Add ProductLabel.swift

---
+++ SmartShop/ProductDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  ProductDetailView.swift
+//  SmartShop
+//
+//  Created by Антон Стафеев on 14.02.2025.
+//
+
+import Foundation
---
+++ SmartShop/QuantityView.swift
@@ -0,0 +1,8 @@
+//
+//  QuantityView.swift
+//  SmartShop
+//
+//  Created by Антон Стафеев on 15.02.2025.
+//
+
+import Foundation
