dd8ee841b1d97090044d9a6b884251c6ccdd19e9
Anton Stafeev <stafeevt@mail.ru>
2025-02-15 15:21:30 +0500

    Add textAlignment property

--- SmartShop/ProductLabel.swift
+++ SmartShop/ProductLabel.swift
@@ -8,13 +8,14 @@
 import UIKit
 
 final class ProductLabel: UILabel {
-    init(text: String = "", textColor: UIColor = .black, font: UIFont, numberOfLines: Int = 1) {
+    init(text: String = "", textColor: UIColor = .black, font: UIFont, numberOfLines: Int = 1, textAlignment: NSTextAlignment = .left) {
         super.init(frame: .zero)
         self.text = text
         self.textColor = textColor
         self.font = font
         self.numberOfLines = numberOfLines
         self.translatesAutoresizingMaskIntoConstraints = false
+        self.textAlignment = textAlignment
     }
     
     @available(*, unavailable)
